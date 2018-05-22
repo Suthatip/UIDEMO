04d87cb2a50f6aab7b13b507348508615994cfa0
Suthatip Chuchuay <suthatip867@gmail.com>
2018-05-22 16:46:42 +0700

    Connection Update

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConnection {
-    private static final String SQCONN = "jdbc:sqlite:mit:sqlite";
+    private static final String SQCONN = "jdbc:sqlite:mit.sqlite";
 
     public static Connection getConnection () throws SQLException {
         try {
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,11 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import javax.swing.*;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -21,28 +23,49 @@ public class Controller implements Initializable {
     private PasswordField password;
     @FXML
     private Button btnLogin;
+    @FXML
+    private Label dbStatus;
+
+    private loginModel loginModel = new loginModel();
 
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        if (this.loginModel.isDatabaseConnection()){
+            this.dbStatus.setText("Connected to DB.");
+        }
+        else {
+            this.dbStatus.setText("Not Connect to DB.");
+        }
 
 
 
     }
     @FXML
-    public void Login(ActionEvent event){
+    public void Login(ActionEvent event) {
+//        try {
+//            if (id.equals(username.getText()) && pass.equals(password.getText())) {
+//                JOptionPane.showMessageDialog(null, "Welcome To MIT SAIYAI APP");
+//            } else {
+//                JOptionPane.showMessageDialog(null,"You username or password is invalid.","Warning Message",JOptionPane.WARNING_MESSAGE);
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
         try {
-            if (id.equals(username.getText()) && pass.equals(password.getText())) {
+            if (this.loginModel.isLogin(this.username.getText(), this.password.getText())) {
                 JOptionPane.showMessageDialog(null, "Welcome To MIT SAIYAI APP");
+
             } else {
-                JOptionPane.showMessageDialog(null,"You username or password is invalid.","Warning Message",JOptionPane.WARNING_MESSAGE);
+                JOptionPane.showMessageDialog(null, "You username or password is invalid.", "Warning Message", JOptionPane.WARNING_MESSAGE);
             }
-        }catch (Exception e){
+
+        }//login
+        catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
-    }//login
 
-
-}//class
+    }//class
